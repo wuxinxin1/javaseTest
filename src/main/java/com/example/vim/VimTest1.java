68e861926724c9a8150002af08751d8643d87dd3
wuxinxin-personal <45267308+wuxinxin-personal@users.noreply.github.com>
2019-07-23 23:29:06 +0800

    vim学习笔记

---
+++ src/main/java/com/example/vim/VimTest1.java
@@ -0,0 +1,21 @@
+package com.example.vim;
+
+/**
+ * Created by Administrator on 2019/7/23/023.
+ */
+public class VimTest1 {
+    public static void main(String[] args) {
+
+        /**
+         * i: 插入当前光标前一个位置
+         * a：插入当前光标后一个位置
+         * o: 插入当前光标下一行
+         *
+         * I:插入当前行最前面
+         * A:插入当前行最末尾
+         * O:插入当前行上一行
+         */
+        //aaaibvaa
+
+    }
+}
