24411742e4b66f3ae946f5985836bd02fc1c2e4e
wuxinxin-personal <45267308+wuxinxin-personal@users.noreply.github.com>
2019-07-24 00:15:51 +0800

    vim学习笔记

--- src/main/java/com/example/vim/VimTest1.java
+++ src/main/java/com/example/vim/VimTest1.java
@@ -17,5 +17,37 @@ public class VimTest1 {
          */
         //aaaibvaa
 
+        /**
+         * 模式：
+         * 1.nomal 模式 用于多个模式切换
+         * 2.command 模式 输入一些命令  wq   vs/sp
+         * 3.insert 模式 进行编辑
+         * 4.可视化 模式  块状选择，批量操作  v 选择单个字符   V 选择行  ctr+v选择块
+         */
+         //进行模式的说明
+         //aaa
+         //bbb
+
+        /**
+         * 删除
+         * 1.ctr+h  删除一个字符
+         * 2.ctr+w  删除一个单词
+         * 3.ctr+u  删除当前行
+         */
+         //my name is wxx
+        //我的名字是wxx
+
+        /**
+         * nomal和insert模式方便切换
+         * 1.转换nomal  ctr+[  直接切换为nomal模式
+         * 2.转换insert gi 跳转到编辑处
+         *
+         *
+         */
+        //模式的切换
+        /**
+         * 上下左右移动 j 下 k 上 l 右 h 左
+         */
+
     }
 }
